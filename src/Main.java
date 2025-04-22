129c53e58d2126bbae7795625ee10593f8ccf8b4
KateLakhti <lahtionovak@gmail.com>
2025-04-21 19:44:19 -0500

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello Batch8!");
+        System.out.println("How are you doing?");
     }
 }
