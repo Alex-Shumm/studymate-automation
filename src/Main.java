3c6a65494f63cf58a31d2a22c108dde9dad1647b
KateLakhti <lahtionovak@gmail.com>
2025-04-21 20:41:03 -0500

    next step

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Batch8!");
         System.out.println("How are you doing?");
+        System.out.println("How are you doing?");
+        System.out.println("How are you doing?");
+        System.out.println("How are you doing?");
+        System.out.println("How are you doing?");
     }
 }
