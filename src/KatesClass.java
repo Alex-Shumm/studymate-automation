3e4bf617ba60c7574a63bc7fe2d894f6423a67f2
KateLakhti <lahtionovak@gmail.com>
2025-04-21 19:46:45 -0500

    second

--- src/KatesClass.java
+++ src/KatesClass.java
@@ -1,2 +1,7 @@
 public class KatesClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello Batch8!");
+        System.out.println("How are you doing?");
+    }
 }
